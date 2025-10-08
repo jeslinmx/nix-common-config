dc2decf2d39383d70b08e313da077561fbe6f8e6
Jeshua Lin <jeslinmx@users.noreply.github.com>
2025-10-08 18:22:47 +0800

    fix(fish): removed l system-wide alias

--- modules/nixos/base/common.nix
+++ modules/nixos/base/common.nix
@@ -16,18 +16,21 @@ flake @ {nixosModules, ...}: {
       base-systemd-boot
       ;
   };
-  config = lib.mkOverride 900 {
-    system.nixos.label = "${config.networking.hostName}-${toString (flake.shortRev or flake.dirtyShortRev or flake.lastModified or "(unknown rev)")}";
-    nixpkgs.config.allowUnfree = true;
-    hardware = {
-      enableRedistributableFirmware = true;
-      cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
-      cpu.amd.updateMicrocode = config.hardware.enableRedistributableFirmware;
-    };
-    users.defaultUserShell = pkgs.fish;
-    programs = {
-      fish.enable = true;
-      wireshark.enable = true;
-    };
-  };
+  config = lib.mkMerge [
+    (lib.mkOverride 900 {
+      system.nixos.label = "${config.networking.hostName}-${toString (flake.shortRev or flake.dirtyShortRev or flake.lastModified or "(unknown rev)")}";
+      nixpkgs.config.allowUnfree = true;
+      hardware = {
+        enableRedistributableFirmware = true;
+        cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
+        cpu.amd.updateMicrocode = config.hardware.enableRedistributableFirmware;
+      };
+      users.defaultUserShell = pkgs.fish;
+      programs = {
+        fish.enable = true;
+        wireshark.enable = true;
+      };
+    })
+    {environment.shellAliases.l = lib.mkForce null;} # get rid of this alias
+  ];
 }

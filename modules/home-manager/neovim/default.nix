27d2267d5708b91319148c83bea75082a160f1bc
Jeshua Lin <jeslinmx@users.noreply.github.com>
2025-10-28 14:59:51 +0800

    fix(neovim): apply config when stylix absent

--- modules/home-manager/neovim/default.nix
+++ modules/home-manager/neovim/default.nix
@@ -5,17 +5,20 @@ _: {
   ...
 }: {
   programs.neovim = {
-    extraLuaConfig = lib.mkIf (builtins.hasAttr "stylix" config) (with config.lib.stylix.colors.withHashtag;
-      ''
-        _G.palette = {
-          base00 = '${base00}', base01 = '${base01}', base02 = '${base02}', base03 = '${base03}',
-          base04 = '${base04}', base05 = '${base05}', base06 = '${base06}', base07 = '${base07}',
-          base08 = '${base08}', base09 = '${base09}', base0A = '${base0A}', base0B = '${base0B}',
-          base0C = '${base0C}', base0D = '${base0D}', base0E = '${base0E}', base0F = '${base0F}'
-        }
-        _G.config_is_hm = true
-      ''
-      + builtins.readFile ./init.lua);
+    extraLuaConfig = (
+      if builtins.hasAttr "stylix" config
+      then (with config.lib.stylix.colors.withHashtag;
+        ''
+          _G.palette = {
+            base00 = '${base00}', base01 = '${base01}', base02 = '${base02}', base03 = '${base03}',
+            base04 = '${base04}', base05 = '${base05}', base06 = '${base06}', base07 = '${base07}',
+            base08 = '${base08}', base09 = '${base09}', base0A = '${base0A}', base0B = '${base0B}',
+            base0C = '${base0C}', base0D = '${base0D}', base0E = '${base0E}', base0F = '${base0F}'
+          }
+          _G.config_is_hm = true
+        '')
+      else ""
+    ) + builtins.readFile ./init.lua;
     extraPackages = builtins.attrValues {
       inherit
         (pkgs)

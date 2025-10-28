4d4f299805b0d6a8eed5ba00b21786cf4d4dda94
Jeshua Lin <jeslinmx@users.noreply.github.com>
2025-10-28 13:16:46 +0800

    fix(cli-programs): option name changes

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -3,80 +3,85 @@ _: {
   pkgs,
   ...
 }: {
-  programs.git = {
+  programs = {
+    git = {
+      settings = {
+        alias = {
+          a = "add";
+          adog = "log --all --decorate --oneline --graph";
+          b = "branch";
+          c = "commit -m";
+          ca = "commit -am";
+          cl = "clone";
+          co = "checkout";
+          d = "diff";
+          f = "fetch";
+          l = "log --ext-diff";
+          m = "merge";
+          r = "restore";
+          ra = "remote add";
+          s = "status";
+          sh = "show --ext-diff";
+          st = "stash";
+          sw = "switch";
+          unstage = "restore --staged";
+        };
+        color.ui = "auto";
+        init.defaultBranch = "main";
+        diff = {
+          mnemonicPrefix = true;
+          renames = true;
+          submodule = "log";
+        };
+        grep = {
+          break = true;
+          heading = true;
+          lineNumber = true;
+          extendedRegexp = true;
+        };
+        pull.ff = "only";
+        status = {
+          submoduleSummary = true;
+          showUntrackedFiles = "all";
+        };
+        tag.sort = "version:refname";
+        url = {
+          "https://github.com/".insteadOf = "gh://";
+          "https://gist.github.com/".insteadOf = "gist://";
+          "git@github.com:".insteadOf = "gh:";
+        };
+        core = {
+          autocrlf = "input";
+          excludesFile =
+            lib.pipe [
+              # neovim
+              "Session.vim"
+              # macos
+              ".DS_Store"
+              ".Spotlight-V100"
+              ".fseventsd"
+              # windows
+              "Thumbs.db"
+              "Desktop.ini"
+              # linux
+              ".Trash-*"
+              ".direnv"
+              "**.swp"
+              "**.swo"
+            ] [
+              (builtins.concatStringsSep "\n")
+              (pkgs.writeText ".gitignore")
+              (builtins.getAttr "outPath")
+            ];
+        };
+      };
+    };
     difftastic = {
       enable = true;
-      display = "side-by-side";
-      background = "dark";
-    };
-    aliases = {
-      a = "add";
-      adog = "log --all --decorate --oneline --graph";
-      b = "branch";
-      c = "commit -m";
-      ca = "commit -am";
-      cl = "clone";
-      co = "checkout";
-      d = "diff";
-      f = "fetch";
-      l = "log --ext-diff";
-      m = "merge";
-      r = "restore";
-      ra = "remote add";
-      s = "status";
-      sh = "show --ext-diff";
-      st = "stash";
-      sw = "switch";
-      unstage = "restore --staged";
-    };
-    extraConfig = {
-      color.ui = "auto";
-      init.defaultBranch = "main";
-      diff = {
-        mnemonicPrefix = true;
-        renames = true;
-        submodule = "log";
-      };
-      grep = {
-        break = true;
-        heading = true;
-        lineNumber = true;
-        extendedRegexp = true;
-      };
-      pull.ff = "only";
-      status = {
-        submoduleSummary = true;
-        showUntrackedFiles = "all";
-      };
-      tag.sort = "version:refname";
-      url = {
-        "https://github.com/".insteadOf = "gh://";
-        "https://gist.github.com/".insteadOf = "gist://";
-        "git@github.com:".insteadOf = "gh:";
-      };
-      core = {
-        autocrlf = "input";
-        excludesFile =
-          lib.pipe [
-            # neovim
-            "Session.vim"
-            # macos
-            ".DS_Store"
-            ".Spotlight-V100"
-            ".fseventsd"
-            # windows
-            "Thumbs.db"
-            "Desktop.ini"
-            # linux
-            ".Trash-*"
-            ".direnv"
-            "**.swp"
-            "**.swo"
-          ] [
-            (builtins.concatStringsSep "\n")
-            (pkgs.writeText ".gitignore")
-            (builtins.getAttr "outPath")
-          ];
+      git.enable = true;
+      options = {
+        display = "side-by-side";
+        background = "dark";
       };
     };
   };

f79d8af37d09b112aa773f3370bcf1f4b739b03b
Jeshua Lin <jeslinmx@users.noreply.github.com>
2025-11-17 18:25:01 +0800

    feat(yubikey): module for u2f-based session auth and gpg

---
+++ modules/nixos/extra/yubikey.nix
@@ -0,0 +1,23 @@
+_: {pkgs, ...}: {
+  services = {
+    udev = {
+      packages = [pkgs.yubikey-personalization];
+      extraRules = ''
+        ACTION=="remove",\
+        ENV{ID_BUS}=="usb",\
+        ENV{ID_MODEL_ID}=="0407",\
+        ENV{ID_VENDOR_ID}=="1050",\
+        ENV{ID_VENDOR}=="Yubico",\
+        RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+      '';
+    };
+  };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+  security.pam.services = {
+    login.u2fAuth = true;
+    sudo.u2fAuth = true;
+  };
+}

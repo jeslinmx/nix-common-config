8f63de95684a30d4ed0ea79eae597025d30cad87
Jeshua Lin <jeslinmx@users.noreply.github.com>
2025-11-19 13:08:34 +0800

    feat(neovim): colorful-winsep

---
+++ modules/home-manager/neovim/lua/plugins/colorful-winsep.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-zh/colorful-winsep.nvim",
+
+  opts = {
+    border = "rounded",
+    indicator_for_2wins = {
+      symbols = {
+        start_left = "╭",
+        end_left = "╰",
+        start_down = "╰",
+        end_down = "╯",
+        start_up = "╭",
+        end_up = "╮",
+        start_right = "╮",
+        end_right = "╯",
+      },
+    },
+  },
+
+  event = "VeryLazy",
+}

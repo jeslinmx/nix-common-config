20bf0de7eeb74f3a96d16b744c55cb69a9470973
Jeshua Lin <jeslinmx@users.noreply.github.com>
2025-12-01 18:40:30 +0800

    feat(neovim): conform hunk formatting

--- modules/home-manager/neovim/lua/plugins/conform.lua
+++ modules/home-manager/neovim/lua/plugins/conform.lua
@@ -1,20 +1,32 @@
 vim.api.nvim_create_user_command("FormatDisable", function(args)
   if args.bang then
-    -- FormatDisable! will disable formatting just for this buffer
     vim.b.disable_autoformat = true
   else
     vim.g.disable_autoformat = true
   end
-end, {
-  desc = "Disable autoformat-on-save",
-  bang = true,
-})
-vim.api.nvim_create_user_command("FormatEnable", function()
-  vim.b.disable_autoformat = false
-  vim.g.disable_autoformat = false
-end, {
-  desc = "Enable autoformat-on-save",
-})
+end, { desc = "Disable autoformat-on-save" })
+vim.api.nvim_create_user_command("FormatEnable", function(args)
+  if args.bang then
+    vim.b.disable_autoformat = false
+  else
+    vim.g.disable_autoformat = false
+  end
+end, { desc = "Enable autoformat-on-save" })
+
+vim.api.nvim_create_user_command("PartialFormatDisable", function(args)
+  if args.bang then
+    vim.b.disable_partial_autoformat = true
+  else
+    vim.g.disable_partial_autoformat = true
+  end
+end, { desc = "Format entire file on save" })
+vim.api.nvim_create_user_command("PartialFormatEnable", function(args)
+  if args.bang then
+    vim.b.disable_partial_autoformat = false
+  else
+    vim.g.disable_partial_autoformat = false
+  end
+end, { desc = "Format only modified hunks on save" })
 
 return {
   "stevearc/conform.nvim",
@@ -41,11 +53,44 @@ return {
     },
 
     format_on_save = function(bufnr)
-      -- Disable with a global or buffer-local variable
+      local conform_opts = { timeout_ms = 500, lsp_format = "fallback" }
+
       if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
         return
       end
-      return { timeout_ms = 500, lsp_format = "fallback" }
+
+      local range_ignore_filetypes = { "lua" }
+      if
+        vim.tbl_contains(range_ignore_filetypes, vim.bo.filetype)
+        or vim.g.disable_partial_autoformat
+        or vim.b[bufnr].disable_partial_autoformat
+      then
+        return conform_opts
+      end
+
+      local hunks = (require("mini.diff").get_buf_data() or {}).hunks
+      if not hunks then
+        vim.notify("No hunks in this buffer to format.", vim.log.levels.INFO, { title = "Conform" })
+        return
+      end
+
+      local format = require("conform").format
+      local function format_range()
+        local hunk = table.remove(hunks)
+        if hunk == nil or hunk.type == "delete" then
+          return
+        end
+
+        local start = hunk.buf_start
+        local last = start + hunk.buf_count - 1
+        -- nvim_buf_get_lines uses zero-based indexing -> subtract from last
+        local last_hunk_line = vim.api.nvim_buf_get_lines(0, last - 1, last, true)[1]
+        local range = { start = { start, 0 }, ["end"] = { last, last_hunk_line:len() } }
+        format({ range = range, timeout = 500, lsp_format = "fallback" }, function()
+          vim.schedule(format_range)
+        end)
+      end
+      format_range()
     end,
   },
 

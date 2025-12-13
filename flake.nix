4e118790b1d097deb0ec0720fd9c1bddd0092cf6
Jeshua Lin <jeslinmx@users.noreply.github.com>
2025-12-13 20:07:18 +0800

    refactor(flake): boilerplate -> common-config

--- flake.nix
+++ flake.nix
@@ -4,14 +4,15 @@
   inputs = {
     # flake helpers
     flake-parts.url = "github:hercules-ci/flake-parts";
-    devshell = {
-      url = "github:numtide/devshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
 
     # nixpkgs
     nixpkgs.url = "nixpkgs/master";
     nixpkgs-darwin.url = "nixpkgs/master";
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+    };
 
     # NixOS modules
     home-manager = {
@@ -28,6 +29,10 @@
     };
 
     # Darwin modules
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs-darwin";
+    };
     home-manager-darwin = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs-darwin";
@@ -60,84 +65,21 @@
     };
   };
 
-  outputs = {
-    flake-parts,
-    devshell,
-    ...
-  } @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} ({
-      self,
-      lib,
-      ...
-    }: {
-      imports = [
-        devshell.flakeModule
-      ];
-
+  outputs = {flake-parts, ...} @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} ({self, ...}: {
       flake = let
         inherit (self.lib) gatherModules;
       in {
+        lib = import ./lib.nix inputs;
+
         darwinModules = gatherModules self ./modules/darwin;
         nixosModules = gatherModules self ./modules/nixos;
         homeModules = gatherModules self ./modules/home-manager;
-
-        lib = {
-          dirToAttrs = path:
-            lib.pipe path [
-              builtins.readDir
-              (builtins.mapAttrs (
-                name: type: let
-                  fullPath = /${path}/${name};
-                in
-                  if type == "directory"
-                  then self.lib.dirToAttrs fullPath
-                  else fullPath
-              ))
-            ];
-          filterNonNix = lib.filterAttrsRecursive (_: v: !(builtins.isPath v) || lib.hasSuffix ".nix" "${v}");
-          filterEmptySubdirs = lib.filterAttrsRecursive (_: v: builtins.isPath v || builtins.length (builtins.attrNames v) != 0);
-          flattenAttrs = let
-            recurse = sep: prefix:
-              lib.foldlAttrs (
-                acc: n: v: let
-                  fullPath = builtins.concatStringsSep sep (prefix ++ [n]);
-                in
-                  acc
-                  // (
-                    if builtins.isPath v
-                    then {${fullPath} = v;}
-                    else recurse sep [fullPath] v
-                  )
-              ) {};
-          in
-            sep: recurse sep [];
-          truncateSuffix = lib.mapAttrs' (n: v: lib.nameValuePair (lib.pipe n [(lib.removeSuffix ".nix") (lib.removeSuffix "-default")]) v);
-          gatherModules = flake:
-            lib.flip lib.pipe [
-              self.lib.dirToAttrs
-              self.lib.filterNonNix
-              self.lib.filterEmptySubdirs
-              (self.lib.flattenAttrs "-")
-              self.lib.truncateSuffix
-              (builtins.mapAttrs (_: import))
-              (builtins.mapAttrs (_: v: v flake))
-            ];
-        };
       };
 
       systems = ["x86_64-linux" "aarch64-darwin"];
       perSystem = {pkgs, ...}: {
         formatter = pkgs.alejandra;
-
-        devshells.default = {
-          commands = [
-            {
-              package = pkgs.nurl;
-              category = "dev";
-            }
-          ];
-          packages = [pkgs.nixd];
-        };
       };
     });
 }
---
+++ lib.nix
@@ -0,0 +1,65 @@
+{
+  nixpkgs,
+  nix-darwin,
+  ...
+}: let
+  lib = nixpkgs.lib;
+in rec {
+  dirToAttrs = path:
+    lib.pipe path [
+      builtins.readDir
+      (builtins.mapAttrs (
+        name: type: let
+          fullPath = /${path}/${name};
+        in
+          if type == "directory"
+          then dirToAttrs fullPath
+          else fullPath
+      ))
+    ];
+
+  filterNonNix = lib.filterAttrsRecursive (_: v: !(builtins.isPath v) || lib.hasSuffix ".nix" "${v}");
+
+  filterEmptySubdirs = lib.filterAttrsRecursive (_: v: builtins.isPath v || builtins.length (builtins.attrNames v) != 0);
+
+  flattenAttrs = let
+    recurse = sep: prefix:
+      lib.foldlAttrs (
+        acc: n: v: let
+          fullPath = builtins.concatStringsSep sep (prefix ++ [n]);
+        in
+          acc
+          // (
+            if builtins.isPath v
+            then {${fullPath} = v;}
+            else recurse sep [fullPath] v
+          )
+      ) {};
+  in
+    sep: recurse sep [];
+  truncateSuffix = lib.mapAttrs' (n: v: lib.nameValuePair (lib.pipe n [(lib.removeSuffix ".nix") (lib.removeSuffix "-default")]) v);
+
+  gatherModules = flake:
+    lib.flip lib.pipe [
+      dirToAttrs
+      filterNonNix
+      filterEmptySubdirs
+      (flattenAttrs "-")
+      truncateSuffix
+      (builtins.mapAttrs (_: import))
+      (builtins.mapAttrs (_: v: v flake))
+    ];
+
+  mkDarwinConfigurations = outerFlake:
+    builtins.mapAttrs (name: v:
+      nix-darwin.lib.darwinSystem {
+        modules = [{networking.hostName = name;} ((import v) outerFlake)];
+      });
+
+  mkNixosConfigurations = outerFlake:
+    builtins.mapAttrs (name: v:
+      lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [{networking.hostName = name;} ((import v) outerFlake)];
+      });
+}

19c14d0175a050723766f563149d09a17157a25b
Jeshua Lin <jeslinmx@users.noreply.github.com>
2026-02-06 15:56:00 +0800

    fix(neovim): use absolute path for nvr

--- modules/nvf/nvr.nix
+++ modules/nvf/nvr.nix
@@ -1,8 +1,14 @@
-_: {pkgs, ...}: {
+_: {
+  lib,
+  pkgs,
+  ...
+}: {
   vim = {
-    luaConfigRC.nvr = ''
-      vim.env.EDITOR = "nvr -l --remote-wait"
-      vim.env.MANPAGER = "nvr -c Man!"
+    luaConfigRC.nvr = let
+      nvr = lib.getExe' pkgs.neovim-remote "nvr";
+    in ''
+      vim.env.EDITOR = "${nvr} -l --remote-wait"
+      vim.env.MANPAGER = "${nvr} -c Man!"
     '';
     extraPackages = [pkgs.neovim-remote];
   };

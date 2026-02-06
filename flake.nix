629de68310b709c758e80d11dbc7e8e3b11797a2
Jeshua Lin <jeslinmx@users.noreply.github.com>
2026-02-06 15:53:55 +0800

    refactor(server): proxy,secrets,backup -> common

--- flake.nix
+++ flake.nix
@@ -29,6 +29,10 @@
       url = "github:caelestia-dots/shell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Darwin modules
     nix-darwin = {

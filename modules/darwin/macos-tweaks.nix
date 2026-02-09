089db0950f79a3b689ec6f8d46dbf2be6cb6c067
Jeshua Lin <jeslinmx@users.noreply.github.com>
2026-02-09 18:28:57 +0800

    fix(macos): fixed a bunch of defaults

--- modules/darwin/macos-tweaks.nix
+++ modules/darwin/macos-tweaks.nix
@@ -1,38 +1,48 @@
 _: _: {
   system.defaults = {
+    ".GlobalPreferences"."com.apple.mouse.scaling" = -1.0; # disable mouse accel
     controlcenter = {
       AirDrop = false;
       BatteryShowPercentage = true;
       Bluetooth = true;
       Display = false;
       FocusModes = true;
-      NowPlaying = true;
+      NowPlaying = false;
       Sound = true;
     };
     dock = {
-      autohide = true;
-      autohide-time-modifier = 0.7;
-      expose-animation-duration = 0.7;
+      appswitcher-all-displays = true;
+      autohide-time-modifier = 0.2;
+      expose-animation-duration = 0.2;
       magnification = true; # on hover
       minimize-to-application = true;
       mru-spaces = false; # do not rearrange spaces by recent usage
+      orientation = "right";
       scroll-to-open = true;
       showhidden = true; # hidden applications have translucent icons
-      slow-motion-allowed = true; # hold shify key while minimizing
+      tilesize = 48; # icon size
+      largesize = 64;
     };
     finder = {
+      _FXEnableColumnAutoSizing = true;
+      _FXShowPosixPathInTitle = true;
       _FXSortFoldersFirst = true;
       _FXSortFoldersFirstOnDesktop = true;
       AppleShowAllExtensions = true; # file extensions in Finder
       FXDefaultSearchScope = "SCcf"; # scope search to current folder by default
       FXPreferredViewStyle = "clmv"; # column view by default
+      ShowMountedServersOnDesktop = true;
       ShowPathbar = true;
       ShowStatusBar = true;
     };
     hitoolbox.AppleFnUsageType = "Change Input Source"; # Fn key switches languages
-    iCal.CalendarSidebarShown = true;
-    universalaccess.closeViewScrollWheelToggle = true; # ctrl+scroll to zoom
+    iCal = {
+      "first day of week" = "Sunday";
+      "TimeZone support enabled" = true;
+      CalendarSidebarShown = true;
+    };
     loginwindow = {
+      DisableConsoleAccess = true;
       GuestEnabled = false;
       PowerOffDisabledWhileLoggedIn = true;
       RestartDisabledWhileLoggedIn = true;
@@ -46,8 +56,21 @@ _: _: {
       ShowSeconds = true;
     };
     NSGlobalDomain = {
+      NSAutomaticCapitalizationEnabled = false;
+      NSAutomaticInlinePredictionEnabled = false;
+      NSAutomaticPeriodSubstitutionEnabled = false;
+      NSAutomaticQuoteSubstitutionEnabled = false;
+      NSAutomaticSpellingCorrectionEnabled = false;
+      NSNavPanelExpandedStateForSaveMode = true;
+      NSNavPanelExpandedStateForSaveMode2 = true;
+      NSStatusItemSelectionPadding = 4;
+      NSStatusItemSpacing = 8;
+      PMPrintingExpandedStateForPrint = true;
+      PMPrintingExpandedStateForPrint2 = true;
       NSWindowShouldDragOnGesture = true;
       "com.apple.keyboard.fnState" = false; # Fn keys function as media keys
+      "com.apple.mouse.tapBehavior" = 1;
+      "com.apple.sound.beep.feedback" = 1;
       "com.apple.springing.enabled" = true;
       "com.apple.springing.delay" = 0.7; # spring directories open faster
       "com.apple.swipescrolldirection" = true; # natural scrolling
@@ -56,7 +79,7 @@ _: _: {
       AppleEnableSwipeNavigateWithScrolls = true;
       AppleICUForce24HourTime = true;
       AppleInterfaceStyle = "Dark";
-      AppleKeyboardUIMode = 3; # tab changes focus
+      AppleKeyboardUIMode = 2; # tab changes focus
       _HIHideMenuBar = false;
       AppleMeasurementUnits = "Centimeters";
       AppleTemperatureUnit = "Celsius";
@@ -66,15 +89,22 @@ _: _: {
       AppleShowScrollBars = "Automatic";
     };
     screencapture = {
+      disable-shadow = true;
       location = "~/Pictures/Screenshots";
       show-thumbnail = true;
+      target = "clipboard";
     };
     trackpad = {
       Clicking = true;
-      Dragging = true;
+      Dragging = true; # double-tap to drag
+      DragLock = true; # tap again to release
       FirstClickThreshold = 0; # less pressure for clicking
       SecondClickThreshold = 0; # less pressure for force-clicking
-      TrackpadThreeFingerDrag = false;
+      TrackpadFourFingerPinchGesture = 2; # spread to show desktop, pinch for launchpad
+      TrackpadPinch = true; # two finger pinch zoom
+      TrackpadThreeFingerTapGesture = 0; # disable 3 finger tap for look up
+      TrackpadTwoFingerDoubleTapGesture = true; # double-tap with 2 fingers for smart zoom
+      TrackpadTwoFingerFromRightEdgeSwipeGesture = 3; # 2 finger right edge swipe for notification centre
       TrackpadRightClick = true;
     };
     WindowManager = {
@@ -83,15 +113,6 @@ _: _: {
       EnableTilingByEdgeDrag = true;
       EnableTilingOptionAccelerator = true; # alt+arrows to tile
       EnableTopTilingByEdgeDrag = true;
-    };
-    CustomUserPreferences = {
-      NSGlobalDomain = {
-        NSToolbarTitleViewRolloverDelay = 0; # instantly show the folder icon on toolbar hover
-      };
-      ".GlobalPreferences" = {
-        "com.apple.mouse.scaling" = 1.0;
-        "com.apple.mouse.linear" = 1; # disable mouse accel
-      };
     };
   };
 }

44e06aa4432cbde272354cd5c2cf1028b5969d06
Jeshua Lin <jeslinmx@users.noreply.github.com>
2026-02-13 17:44:38 +0800

    refactor(caddy): use common options

--- modules/nixos/server/common/proxy.nix
+++ modules/nixos/server/common/proxy.nix
@@ -4,38 +4,50 @@ _: {
   pkgs,
   ...
 }: {
-  config = {
-    services.caddy = {
-      enable =
-        config.server
-        |> builtins.attrValues
-        |> builtins.any (service: service.proxy.enable);
-      package = pkgs.caddy.withPlugins {
-        plugins = ["github.com/caddy-dns/cloudflare@v0.2.2"];
-        hash = "sha256-dnhEjopeA0UiI+XVYHYpsjcEI6Y1Hacbi28hVKYQURg=";
+  config = lib.mkMerge [
+    {
+      server.caddy = {
+        proxy.enable = false;
+        backup.enable = false;
+        sopsKeys = {
+          cloudflareApiToken = lib.mkDefault "";
+        };
       };
-      # for testing
-      # acmeCA = "https://acme-staging-v02.api.letsencrypt.org/directory";
-      enableReload = false; # since admin API is disabled
-      globalConfig = ''
-        admin off
-        grace_period 10s
-        acme_dns cloudflare {env.CF_API_TOKEN}
-        servers {
-          metrics
-        }
-      '';
-    };
-    # trigger caddy restart on re-config
-    systemd.services.caddy.restartTriggers = [config.services.caddy.configFile];
-    # get cloudflare api token from sops
-    systemd.services.caddy.serviceConfig.EnvironmentFile = config.sops.templates.caddy-envFile.path;
-    # open firewall
-    networking.firewall.allowedTCPPorts = lib.mkIf config.services.caddy.enable [80 443];
-    # increase socket buffer size for quic-go
-    boot.kernel.sysctl = {
-      "net.core.rmem_max" = 7500000;
-      "net.core.wmem_max" = 7500000;
-    };
-  };
+    }
+    {
+      services.caddy = {
+        enable =
+          config.server
+          |> builtins.attrValues
+          |> builtins.any (service: service.proxy.enable);
+        package = pkgs.caddy.withPlugins {
+          plugins = ["github.com/caddy-dns/cloudflare@v0.2.2"];
+          hash = "sha256-dnhEjopeA0UiI+XVYHYpsjcEI6Y1Hacbi28hVKYQURg=";
+        };
+        # for testing
+        # acmeCA = "https://acme-staging-v02.api.letsencrypt.org/directory";
+        enableReload = false; # since admin API is disabled
+        globalConfig = ''
+          admin off
+          grace_period 10s
+          acme_dns cloudflare {env.CF_API_TOKEN}
+          servers {
+            metrics
+          }
+        '';
+      };
+    }
+    (lib.mkIf config.services.caddy.enable {
+      # get cloudflare api token from sops
+      systemd.services.caddy.serviceConfig.EnvironmentFile = config.sops.templates.caddyEnvFile.path;
+      sops.templates.caddyEnvFile.content = "CF_API_TOKEN=${config.server.caddy.sopsPlaceholders.cloudflareApiToken}";
+      # open firewall
+      networking.firewall.allowedTCPPorts = [80 443];
+      # increase socket buffer size for quic-go
+      boot.kernel.sysctl = {
+        "net.core.rmem_max" = 7500000;
+        "net.core.wmem_max" = 7500000;
+      };
+    })
+  ];
 }

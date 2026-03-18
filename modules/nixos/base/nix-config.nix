31467cabbfa3f3b27cf6763aba42f33330412fb1
Jeshua Lin <jeslinmx@users.noreply.github.com>
2026-03-18 12:42:28 +0800

    fix(nix): add colmena substituter

--- modules/nixos/base/nix-config.nix
+++ modules/nixos/base/nix-config.nix
@@ -9,9 +9,11 @@
       trusted-users = ["root" "@wheel" "@admin"];
       trusted-substituters = [
         "https://nvf.cachix.org/"
+        "https://colmena.cachix.org"
       ];
       trusted-public-keys = [
         "nvf.cachix.org-1:GMQWiUhZ6ux9D5CvFFMwnc2nFrUHTeGaXRlVBXo+naI="
+        "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
       ];
     };
     optimise.automatic = true;

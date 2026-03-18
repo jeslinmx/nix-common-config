df6a2c2102e925f27fbc62368522a1de1a5c1ba8
Jeshua Lin <jeslinmx@users.noreply.github.com>
2026-03-18 13:50:43 +0800

    feat(neovim): add more opencode keybinds

--- modules/nvf/ui/opencode.nix
+++ modules/nvf/ui/opencode.nix
@@ -33,8 +33,53 @@ _: {
         desc = "ask";
         lua = true;
       })
+      (mkKeymap ["n" "v"] "<leader>oA" ''
+          function()
+            require("opencode").command("session.new")
+            require("opencode").ask()
+          end
+        '' {
+          desc = "ask in new session";
+          lua = true;
+        })
+      (mkKeymap ["n" "v"] "<leader>ot" ''require("opencode").toggle'' {
+        desc = "toggle";
+        lua = true;
+      })
+      (mkKeymap ["n" "v"] "<leader>os" ''require("opencode").select_session'' {
+        desc = "sessions";
+        lua = true;
+      })
+      (mkKeymap ["n" "v"] "<leader>oS" ''require("opencode").select_server'' {
+        desc = "servers";
+        lua = true;
+      })
       (mkKeymap ["n" "v"] "<leader>oo" ''require("opencode").select'' {
-        desc = "select";
+        desc = "picker";
+        lua = true;
+      })
+      (mkKeymap ["n" "v"] "<leader>on" ''function() require("opencode").command("session.new") end'' {
+        desc = "new session";
+        lua = true;
+      })
+      (mkKeymap ["n" "v"] "<leader>ox" ''function() require("opencode").command("session.interrupt") end'' {
+        desc = "interrupt";
+        lua = true;
+      })
+      (mkKeymap ["n" "v"] "<leader>oc" ''function() require("opencode").command("session.compact") end'' {
+        desc = "compact";
+        lua = true;
+      })
+      (mkKeymap ["n" "v"] "<leader>ou" ''function() require("opencode").command("session.undo") end'' {
+        desc = "undo";
+        lua = true;
+      })
+      (mkKeymap ["n" "v"] "<leader>or" ''function() require("opencode").command("session.redo") end'' {
+        desc = "redo";
+        lua = true;
+      })
+      (mkKeymap ["n" "v"] "<leader>o<tab>" ''function() require("opencode").command("agent.cycle") end'' {
+        desc = "agent";
         lua = true;
       })
     ];

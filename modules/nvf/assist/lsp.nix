8cc999756108f67320c5ae467908d856dafebecf
Jeshua Lin <jeslinmx@users.noreply.github.com>
2026-03-18 13:15:23 +0800

    refactor(neovim): move language config

--- modules/nvf/assist/lsp.nix
+++ modules/nvf/assist/lsp.nix
@@ -1,4 +1,8 @@
-_: {...}: {
+_: {
+  lib,
+  pkgs,
+  ...
+}: {
   vim = {
     lsp = {
       enable = true;
@@ -8,6 +12,19 @@ _: {...}: {
     };
     languages = {
       enableExtraDiagnostics = true;
+      nix.enable = true;
+      nix.lsp.servers = ["nixd"];
+      lua.enable = true;
+      html.enable = true;
+      markdown.enable = true;
+      markdown.lsp.servers = lib.mkForce ["markdown-oxide"];
+      css.enable = true;
+      ts.enable = true;
+      json.enable = true;
+      yaml.enable = true;
+      typst.enable = true;
+      python.enable = true;
     };
+    treesitter.grammars = [pkgs.vimPlugins.nvim-treesitter.builtGrammars.latex];
   };
 }
--- modules/nvf/default.nix
+++ modules/nvf/default.nix
@@ -1,30 +1,7 @@
-{nvfModules, ...}: {
-  lib,
-  pkgs,
-  ...
-}: {
+{nvfModules, ...}: {...}: {
   imports = builtins.attrValues (removeAttrs nvfModules ["default"]);
   vim = {
     viAlias = true;
     vimAlias = true;
-    languages = {
-      nix = {
-        enable = true;
-        lsp.servers = ["nixd"];
-      };
-      lua.enable = true;
-      html.enable = true;
-      markdown = {
-        enable = true;
-        lsp.servers = lib.mkForce ["markdown-oxide"];
-      };
-      css.enable = true;
-      ts.enable = true;
-      json.enable = true;
-      yaml.enable = true;
-      typst.enable = true;
-      python.enable = true;
-    };
-    treesitter.grammars = [pkgs.vimPlugins.nvim-treesitter.builtGrammars.latex];
   };
 }

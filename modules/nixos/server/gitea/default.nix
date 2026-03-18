02f7f91e7c72916856fdf0e8ce982d9628ce5ee4
Jeshua Lin <jeslinmx@users.noreply.github.com>
2026-03-18 22:32:23 +0800

    fix(gitea): streamline sso

--- modules/nixos/server/gitea/default.nix
+++ modules/nixos/server/gitea/default.nix
@@ -16,6 +16,7 @@
         ROOT_URL = "https://${config.server.gitea.proxy.address}";
         HTTP_ADDR = "localhost";
         HTTP_PORT = 3001;
+        LANDING_PAGE = "login";
       };
       repository = {
         ACCESS_CONTROL_ALLOW_ORIGIN = config.server.gitea.proxy.address;
@@ -26,7 +27,14 @@
       cors = {
         ALLOW_DOMAIN = config.server.gitea.proxy.address;
       };
-      service.DISABLE_REGISTRATION = true;
+      service = {
+        DISABLE_REGISTRATION = true;
+        ENABLE_OPENID_SIGNUP = true;
+        ENABLE_BASIC_AUTHENTICATION = false;
+        ENABLE_PASSWORD_SIGNIN_FORM = false;
+        ENABLE_PASSKEY_AUTHENTICATION = false;
+        REQUIRE_SIGNIN_VIEW = true;
+      };
       session.COOKIE_SECURE = true;
     };
     database = {

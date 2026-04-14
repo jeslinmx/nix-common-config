3e802fdd93b61531e06500fbafd52e8f0092fd8f
Jeshua Lin <jeslinmx@users.noreply.github.com>
2026-04-14 12:59:54 +0800

    feat(tiny-dfr): add spacers, timer and battery

--- modules/nixos/interactive/tiny-dfr.nix
+++ modules/nixos/interactive/tiny-dfr.nix
@@ -13,100 +13,130 @@
         {
           Text = "F1";
           Action = "F1";
+          Stretch = 3;
         }
         {
           Text = "F2";
           Action = "F2";
+          Stretch = 3;
         }
         {
           Text = "F3";
           Action = "F3";
+          Stretch = 3;
         }
         {
           Text = "F4";
           Action = "F4";
+          Stretch = 3;
         }
+        {Stretch = 1;}
         {
           Text = "F5";
           Action = "F5";
+          Stretch = 3;
         }
         {
           Text = "F6";
           Action = "F6";
+          Stretch = 3;
         }
         {
           Text = "F7";
           Action = "F7";
+          Stretch = 3;
         }
         {
           Text = "F8";
           Action = "F8";
+          Stretch = 3;
         }
+        {Stretch = 1;}
         {
           Text = "F9";
           Action = "F9";
+          Stretch = 3;
         }
         {
           Text = "F10";
           Action = "F10";
+          Stretch = 3;
         }
         {
           Text = "F11";
           Action = "F11";
+          Stretch = 3;
         }
         {
           Text = "F12";
           Action = "F12";
+          Stretch = 3;
         }
+        {Stretch = 1;}
         {
           Text = "PrtSc";
           Action = "Sysrq";
+          Stretch = 3;
         }
       ];
       MediaLayerKeys = [
         {
+          Time = "%e/%m %H:%M";
+          Action = "Time";
+          Stretch = 4;
+        }
+        {
+          Battery = "both";
+          Action = "Battery";
+          Stretch = 3;
+        }
+        {
           Icon = "brightness_low";
           Action = "BrightnessDown";
+          Stretch = 2;
         }
         {
           Icon = "brightness_high";
           Action = "BrightnessUp";
+          Stretch = 2;
         }
-        {
-          Icon = "backlight_low";
-          Action = "IllumDown";
-        }
-        {
-          Icon = "backlight_high";
-          Action = "IllumUp";
-        }
+
+        {Stretch = 1;}
         {
           Icon = "fast_rewind";
           Action = "PreviousSong";
+          Stretch = 2;
         }
         {
           Icon = "play_pause";
           Action = "PlayPause";
+          Stretch = 3;
         }
         {
           Icon = "fast_forward";
           Action = "NextSong";
+          Stretch = 2;
         }
+        {Stretch = 1;}
         {
           Icon = "mic_off";
           Action = "MicMute";
+          Stretch = 2;
         }
         {
           Icon = "volume_off";
           Action = "Mute";
+          Stretch = 3;
         }
         {
           Icon = "volume_down";
           Action = "VolumeDown";
+          Stretch = 3;
         }
         {
           Icon = "volume_up";
           Action = "VolumeUp";
+          Stretch = 3;
         }
       ];
     };

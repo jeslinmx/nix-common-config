0a30ecb70cd01c1243286b3afe8928d594debafc
Jeshua Lin <jeslinmx@users.noreply.github.com>
2026-06-15 15:23:23 +0800

    fix(gui-programs): remove switchboard, pavucontrol

--- modules/home-manager/gui-programs.nix
+++ modules/home-manager/gui-programs.nix
@@ -34,35 +34,15 @@
   };
 
   # unnixed stuff
-  home.packages =
-    builtins.attrValues {
-      inherit
-        (pkgs)
-        wl-clipboard
-        # virt-manager
-        clapper
-        loupe
-        zathura
-        ;
-      inherit (pkgs.pantheon) elementary-files;
-    }
-    ++ [
-      pkgs.pavucontrol
-      (pkgs.pantheon.switchboard-with-plugs.override {
-        plugs = builtins.attrValues {
-          inherit
-            (pkgs.pantheon)
-            switchboard-plug-about
-            # broken due to missing org.gnome.settings-daemon.plugins.media-keys schema
-            # switchboard-plug-sound
-            switchboard-plug-network
-            switchboard-plug-printers
-            # pulls in wingpanel indicator and thus all of gnome
-            # switchboard-plug-bluetooth
-            switchboard-plug-applications
-            ;
-        };
-        useDefaultPlugs = false;
-      })
-    ];
+  home.packages = builtins.attrValues {
+    inherit
+      (pkgs)
+      wl-clipboard
+      # virt-manager
+      clapper
+      loupe
+      zathura
+      ;
+    inherit (pkgs.pantheon) elementary-files;
+  };
 }

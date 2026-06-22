0414a036e89f3ffece4e43ab8648a1c65e801630
Jeshua Lin <jeslinmx@users.noreply.github.com>
2026-06-22 19:50:03 +0800

    fix(ghostty): differentiate macos/linux keybinds

--- modules/home-manager/ghostty.nix
+++ modules/home-manager/ghostty.nix
@@ -10,95 +10,83 @@ _: {
       focus-follows-mouse = true;
       quick-terminal-position = "bottom";
       quick-terminal-autohide = false;
-      keybind = [
-        # Tabs
-        "ctrl+tab=next_tab"
-        "ctrl+shift+tab=previous_tab"
+      keybind =
+        {
+          # Tabs
+          "ctrl+tab" = "next_tab";
+          "ctrl+shift+tab" = "previous_tab";
 
-        "alt+one=goto_tab:1"
-        "alt+two=goto_tab:2"
-        "alt+three=goto_tab:3"
-        "alt+four=goto_tab:4"
-        "alt+five=goto_tab:5"
-        "alt+six=goto_tab:6"
-        "alt+seven=goto_tab:7"
-        "alt+eight=goto_tab:8"
-        "alt+nine=last_tab"
+          "opt+one" = "goto_tab:1";
+          "opt+two" = "goto_tab:2";
+          "opt+three" = "goto_tab:3";
+          "opt+four" = "goto_tab:4";
+          "opt+five" = "goto_tab:5";
+          "opt+six" = "goto_tab:6";
+          "opt+seven" = "goto_tab:7";
+          "opt+eight" = "goto_tab:8";
+          "opt+nine" = "last_tab";
 
-        # Splits
-        "ctrl+shift+\\=new_split:right"
-        "ctrl+shift+\"=new_split:down"
-        "cmd+\\=new_split:right"
-        "cmd+\"=new_split:down"
+          # Splits
+          "cmd+\\" = "new_split:right";
+          "cmd+\"" = "new_split:down";
 
-        "ctrl+shift+k=goto_split:up"
-        "ctrl+shift+j=goto_split:down"
-        "ctrl+shift+h=goto_split:left"
-        "ctrl+shift+l=goto_split:right"
-        "cmd+k=goto_split:up"
-        "cmd+j=goto_split:down"
-        "cmd+h=goto_split:left"
-        "cmd+l=goto_split:right"
+          "cmd+k" = "goto_split:up";
+          "cmd+j" = "goto_split:down";
+          "cmd+h" = "goto_split:left";
+          "cmd+l" = "goto_split:right";
 
-        "ctrl+shift+up=resize_split:up,10"
-        "ctrl+shift+down=resize_split:down,10"
-        "ctrl+shift+left=resize_split:left,10"
-        "ctrl+shift+right=resize_split:right,10"
-        "ctrl+shift+plus=equalize_splits"
-        "ctrl+shift+enter=toggle_split_zoom"
-        "cmd+up=resize_split:up,10"
-        "cmd+down=resize_split:down,10"
-        "cmd+left=resize_split:left,10"
-        "cmd+right=resize_split:right,10"
-        "cmd+equal=equalize_splits"
-        "cmd+enter=toggle_split_zoom"
+          "cmd+up" = "resize_split:up,10";
+          "cmd+down" = "resize_split:down,10";
+          "cmd+left" = "resize_split:left,10";
+          "cmd+right" = "resize_split:right,10";
+          "cmd+equal" = "equalize_splits";
+          "cmd+enter" = "toggle_split_zoom";
 
-        # Window
-        "page_up=scroll_page_up"
-        "page_down=scroll_page_down"
-        "shift+page_down=jump_to_prompt:1"
-        "shift+page_up=jump_to_prompt:-1"
-        "ctrl+home=scroll_to_top"
-        "ctrl+end=scroll_to_bottom"
+          # Window
+          "page_up" = "scroll_page_up";
+          "page_down" = "scroll_page_down";
+          "cmd+page_down" = "jump_to_prompt:1";
+          "cmd+page_up" = "jump_to_prompt:-1";
+          "cmd+home" = "scroll_to_top";
+          "cmd+end" = "scroll_to_bottom";
 
-        "ctrl+minus=decrease_font_size:1"
-        "ctrl+equal=increase_font_size:1"
-        "ctrl+zero=reset_font_size"
-        "cmd+minus=decrease_font_size:1"
-        "cmd+equal=increase_font_size:1"
-        "cmd+zero=reset_font_size"
+          "ctrl+minus" = "decrease_font_size:1";
+          "ctrl+equal" = "increase_font_size:1";
+          "ctrl+zero" = "reset_font_size";
 
-        "shift+up=adjust_selection:up"
-        "shift+down=adjust_selection:down"
-        "shift+left=adjust_selection:left"
-        "shift+right=adjust_selection:right"
+          "shift+up" = "adjust_selection:up";
+          "shift+down" = "adjust_selection:down";
+          "shift+left" = "adjust_selection:left";
+          "shift+right" = "adjust_selection:right";
 
-        "ctrl+shift+a=select_all"
-        "ctrl+shift+c=copy_to_clipboard"
-        "ctrl+shift+v=paste_from_clipboard"
-        "ctrl+shift+e=copy_url_to_clipboard"
-        "cmd+a=select_all"
-        "cmd+c=copy_to_clipboard"
-        "cmd+v=paste_from_clipboard"
-        "cmd+e=copy_url_to_clipboard"
-
-        "ctrl+alt+shift+j=write_screen_file:open"
-        "ctrl+shift+j=write_screen_file:paste"
-
-        # General
-        "ctrl+shift+p=toggle_command_palette"
-        "ctrl+shift+n=new_window"
-        "ctrl+shift+t=new_tab"
-        "ctrl+shift+w=close_tab"
-        "ctrl+shift+comma=reload_config"
-        "ctrl+shift+r=reset"
-        "ctrl+shift+i=inspector:toggle"
-        "cmd+p=toggle_command_palette"
-        "cmd+n=new_window"
-        "cmd+t=new_tab"
-        "cmd+w=close_tab"
-        "cmd+q=quit"
-      ];
+          # General
+          "cmd+a" = "select_all";
+          "cmd+c" = "copy_to_clipboard";
+          "cmd+v" = "paste_from_clipboard";
+          "cmd+e" = "copy_url_to_clipboard";
+          "cmd+f" = "start_search";
+          "cmd+p" = "toggle_command_palette";
+          "cmd+n" = "new_window";
+          "cmd+t" = "new_tab";
+          "cmd+w" = "close_tab";
+          "cmd+q" = "quit";
+          "cmd+comma" = "reload_config";
+          "cmd+s" = "write_screen_file:copy";
+          "cmd+r" = "reset";
+          "cmd+i" = "inspector:toggle";
+        }
+        |> lib.mapAttrsToList (
+          trigger: action: let
+            replaceMacModifiers =
+              builtins.replaceStrings
+              ["cmd" "opt"]
+              ["ctrl+shift" "ctrl"];
+          in "${
+            if pkgs.stdenv.isDarwin
+            then trigger
+            else replaceMacModifiers trigger
+          }=${action}"
+        );
     };
   };
 }

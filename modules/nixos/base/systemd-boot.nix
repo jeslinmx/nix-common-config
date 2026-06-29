b3ea95ae93c0cfea32451858d696d9caf7f89b57
Jeshua Lin <jeslinmx@users.noreply.github.com>
2026-06-29 15:43:24 +0800

    feat(limine): add mt86+ and netbootxyz
    - fix conflict with systemd-boot

--- modules/nixos/base/systemd-boot.nix
+++ modules/nixos/base/systemd-boot.nix
@@ -6,7 +6,7 @@ _: {
   boot.loader = {
     grub.enable = false;
     systemd-boot = {
-      enable = true;
+      enable = lib.mkDefault true;
       configurationLimit = 10;
       netbootxyz.enable = lib.meta.availableOn pkgs.stdenv.hostPlatform pkgs.netbootxyz-efi;
       memtest86.enable = lib.meta.availableOn pkgs.stdenv.hostPlatform pkgs.memtest86plus;
--- modules/nixos/extra/secure-boot-limine.nix
+++ modules/nixos/extra/secure-boot-limine.nix
@@ -1,6 +1,7 @@
 _: {
   config,
   lib,
+  pkgs,
   ...
 }: {
   # For secure boot to work, you must do the following steps:
@@ -11,13 +12,38 @@ _: {
   # 3. set Secure Boot back to Deployed Mode if not automatically reverted
 
   boot = {
-    loader.limine = {
-      enable = true;
-      maxGenerations = 10;
-      secureBoot.enable = true;
-      style.wallpapers = lib.mkForce [];
+    loader = {
+      grub.enable = lib.mkForce false;
+      systemd-boot.enable = lib.mkForce false;
+      limine = let
+        efiProg = name: file: let
+          filePath = "efi/${name}/${name}.efi";
+          checksum =
+            pkgs.runCommand "${name}-b2sum" {} ''
+              b2sum ${file} | awk '{printf "%s",$1}' > $out
+            ''
+            |> builtins.readFile;
+        in {
+          extraEntries = ''
+            /${name}
+              protocol: efi
+              path: boot():/limine/${filePath}#${checksum}
+          '';
+          additionalFiles.${filePath} = file;
+        };
+      in
+        lib.mkMerge [
+          {
+            enable = true;
+            maxGenerations = 10;
+            secureBoot.enable = true;
+            style.wallpapers = lib.mkForce [];
+          }
+          (lib.mkIf (lib.meta.availableOn pkgs.stdenv.hostPlatform pkgs.memtest86-efi) (efiProg "memtest86+" "${pkgs.memtest86-efi}/BOOTX64.efi"))
+          (lib.mkIf (lib.meta.availableOn pkgs.stdenv.hostPlatform pkgs.netbootxyz-efi) (efiProg "netbootxyz" pkgs.netbootxyz-efi))
+        ];
+      timeout = 1;
     };
-    loader.timeout = 1;
     # for TPM auto-unlocking
     # note that when the boot environment changes (e.g. UEFI firmware update, new secure boot keys enrolled),
     # the PCRs will be invalidated, and thus the TPM will stop dispensing the previously enrolled key

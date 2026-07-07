6fb7b98df89ca83c7ecb51fe3815e6c11b9172a8
Jeshua Lin <jeslinmx@users.noreply.github.com>
2026-07-07 17:37:34 +0800

    fix(neovim): manpages, finally

--- modules/nvf/nvr.nix
+++ modules/nvf/nvr.nix
@@ -8,7 +8,7 @@ _: {
       nvr = lib.getExe' pkgs.neovim-remote "nvr";
     in ''
       vim.env.EDITOR = "${nvr} -l --remote-wait"
-      vim.env.MANPAGER = "${nvr} -c Man!"
+      vim.env.MANPAGER = "${nvr} -cc 'new' --remote +'Man! | setlocal nobuflisted winfixbuf' -"
     '';
     extraPackages = [pkgs.neovim-remote];
   };
